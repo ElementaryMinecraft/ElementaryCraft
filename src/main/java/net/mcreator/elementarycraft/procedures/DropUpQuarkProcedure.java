27a1050498a198b28c93f53830f68f751d2f6f2f
MHoutput <Matthew@192.168.0.240>
2020-08-08 21:22:22 +0200

    Added up quarks + field

---
+++ src/main/java/net/mcreator/elementarycraft/procedures/DropUpQuarkProcedure.java
@@ -0,0 +1,84 @@
+package net.mcreator.elementarycraft.procedures;
+
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.item.ItemEntity;
+
+import net.mcreator.elementarycraft.item.UpQuarkRedItem;
+import net.mcreator.elementarycraft.item.UpQuarkGreenItem;
+import net.mcreator.elementarycraft.item.UpQuarkBlueItem;
+import net.mcreator.elementarycraft.item.AntiUpQuarkRedItem;
+import net.mcreator.elementarycraft.item.AntiUpQuarkGreenItem;
+import net.mcreator.elementarycraft.item.AntiUpQuarkBlueItem;
+import net.mcreator.elementarycraft.block.UpQuarkFieldBlock;
+import net.mcreator.elementarycraft.ElementaryCraftModElements;
+
+import java.util.Map;
+
+@ElementaryCraftModElements.ModElement.Tag
+public class DropUpQuarkProcedure extends ElementaryCraftModElements.ModElement {
+	public DropUpQuarkProcedure(ElementaryCraftModElements instance) {
+		super(instance, 7);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("x") == null) {
+			System.err.println("Failed to load dependency x for procedure DropUpQuark!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			System.err.println("Failed to load dependency y for procedure DropUpQuark!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			System.err.println("Failed to load dependency z for procedure DropUpQuark!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			System.err.println("Failed to load dependency world for procedure DropUpQuark!");
+			return;
+		}
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		if ((Math.random() <= 0.333333333333333)) {
+			if (!world.getWorld().isRemote) {
+				ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(UpQuarkRedItem.block, (int) (1)));
+				entityToSpawn.setPickupDelay(10);
+				world.addEntity(entityToSpawn);
+			}
+			if (!world.getWorld().isRemote) {
+				ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(AntiUpQuarkRedItem.block, (int) (1)));
+				entityToSpawn.setPickupDelay(10);
+				world.addEntity(entityToSpawn);
+			}
+		} else {
+			if ((Math.random() <= 0.5)) {
+				if (!world.getWorld().isRemote) {
+					ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(UpQuarkBlueItem.block, (int) (1)));
+					entityToSpawn.setPickupDelay(10);
+					world.addEntity(entityToSpawn);
+				}
+				if (!world.getWorld().isRemote) {
+					ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(AntiUpQuarkBlueItem.block, (int) (1)));
+					entityToSpawn.setPickupDelay(10);
+					world.addEntity(entityToSpawn);
+				}
+			} else {
+				if (!world.getWorld().isRemote) {
+					ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(UpQuarkGreenItem.block, (int) (1)));
+					entityToSpawn.setPickupDelay(10);
+					world.addEntity(entityToSpawn);
+				}
+				if (!world.getWorld().isRemote) {
+					ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(AntiUpQuarkGreenItem.block, (int) (1)));
+					entityToSpawn.setPickupDelay(10);
+					world.addEntity(entityToSpawn);
+				}
+			}
+		}
+		world.setBlockState(new BlockPos((int) x, (int) y, (int) z), UpQuarkFieldBlock.block.getDefaultState(), 3);
+	}
+}
