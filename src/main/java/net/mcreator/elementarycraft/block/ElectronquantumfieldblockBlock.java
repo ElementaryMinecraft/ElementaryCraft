bc51f43910f082f1fce57c8aaf7557d9074cf506
TimourIc <User@192.168.0.135>
2020-08-08 22:41:50 +0200

    In deze branch zijn de items "Electron", "Positron" en hun corresponderend kwantumveld aangemaakt.

---
+++ src/main/java/net/mcreator/elementarycraft/block/ElectronquantumfieldblockBlock.java
@@ -0,0 +1,78 @@
+
+package net.mcreator.elementarycraft.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Hand;
+import net.minecraft.util.Direction;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.elementarycraft.procedures.ElectronquantumfieldBlockDropProcedure;
+import net.mcreator.elementarycraft.ElementaryCraftModElements;
+
+import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Collections;
+
+@ElementaryCraftModElements.ModElement.Tag
+public class ElectronquantumfieldblockBlock extends ElementaryCraftModElements.ModElement {
+	@ObjectHolder("elementary_craft:electronquantumfieldblock")
+	public static final Block block = null;
+	public ElectronquantumfieldblockBlock(ElementaryCraftModElements instance) {
+		super(instance, 3);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).lightValue(0));
+			setRegistryName("electronquantumfieldblock");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+
+		@Override
+		public ActionResultType onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity entity, Hand hand,
+				BlockRayTraceResult hit) {
+			super.onBlockActivated(state, world, pos, entity, hand, hit);
+			int x = pos.getX();
+			int y = pos.getY();
+			int z = pos.getZ();
+			Direction direction = hit.getFace();
+			{
+				Map<String, Object> $_dependencies = new HashMap<>();
+				$_dependencies.put("x", x);
+				$_dependencies.put("y", y);
+				$_dependencies.put("z", z);
+				$_dependencies.put("world", world);
+				ElectronquantumfieldBlockDropProcedure.executeProcedure($_dependencies);
+			}
+			return ActionResultType.SUCCESS;
+		}
+	}
+}
