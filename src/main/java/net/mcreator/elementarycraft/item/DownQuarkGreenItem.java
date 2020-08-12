b2c07b0349b691ed6dc480d2089dce7baa1f11be
Matthew <Matthew@192.168.0.240>
2020-08-12 21:38:06 +0200

    MCreator Merge commit '1e3f9aee16d56c68f1a3f0aa7d24d4f8db116217' into 14b07c617ba796ff88eefe4398e79e131e5e31a0

--- src/main/java/net/mcreator/elementarycraft/item/DownQuarkGreenItem.java
+++ src/main/java/net/mcreator/elementarycraft/item/DownQuarkGreenItem.java
@@ -1,19 +1,12 @@
 
 package net.mcreator.elementarycraft.item;
 
-import net.minecraftforge.registries.ObjectHolder;
-
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.Item;
-import net.minecraft.block.BlockState;
-
-import net.mcreator.elementarycraft.ElementaryCraftModElements;
-
 @ElementaryCraftModElements.ModElement.Tag
 public class DownQuarkGreenItem extends ElementaryCraftModElements.ModElement {
+
 	@ObjectHolder("elementary_craft:down_quark_green")
 	public static final Item block = null;
+
 	public DownQuarkGreenItem(ElementaryCraftModElements instance) {
 		super(instance, 8);
 	}
@@ -22,7 +15,9 @@ public class DownQuarkGreenItem extends ElementaryCraftModElements.ModElement {
 	public void initElements() {
 		elements.items.add(() -> new ItemCustom());
 	}
+
 	public static class ItemCustom extends Item {
+
 		public ItemCustom() {
 			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64));
 			setRegistryName("down_quark_green");
@@ -42,5 +37,7 @@ public class DownQuarkGreenItem extends ElementaryCraftModElements.ModElement {
 		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
 			return 1F;
 		}
+
 	}
+
 }
--- src/main/java/net/mcreator/elementarycraft/procedures/DropDownQuarkProcedure.java
+++ src/main/java/net/mcreator/elementarycraft/procedures/DropDownQuarkProcedure.java
@@ -1,29 +1,11 @@
 package net.mcreator.elementarycraft.procedures;
 
-import net.minecraft.world.IWorld;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.item.ItemStack;
-import net.minecraft.entity.item.ItemEntity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.Entity;
-import net.minecraft.enchantment.Enchantments;
-import net.minecraft.enchantment.EnchantmentHelper;
-
-import net.mcreator.elementarycraft.item.DownQuarkRedItem;
-import net.mcreator.elementarycraft.item.DownQuarkGreenItem;
-import net.mcreator.elementarycraft.item.DownQuarkBlueItem;
-import net.mcreator.elementarycraft.item.AntiDownQuarkRedItem;
-import net.mcreator.elementarycraft.item.AntiDownQuarkGreenItem;
-import net.mcreator.elementarycraft.item.AntiDownQuarkBlueItem;
-import net.mcreator.elementarycraft.block.DownQuarkFieldBlock;
-import net.mcreator.elementarycraft.ElementaryCraftModElements;
-
-import java.util.Map;
-
 @ElementaryCraftModElements.ModElement.Tag
 public class DropDownQuarkProcedure extends ElementaryCraftModElements.ModElement {
+
 	public DropDownQuarkProcedure(ElementaryCraftModElements instance) {
 		super(instance, 15);
+
 	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
@@ -47,11 +29,13 @@ public class DropDownQuarkProcedure extends ElementaryCraftModElements.ModElemen
 			System.err.println("Failed to load dependency world for procedure DropDownQuark!");
 			return;
 		}
+
 		Entity entity = (Entity) dependencies.get("entity");
 		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
 		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
 		IWorld world = (IWorld) dependencies.get("world");
+
 		if (((EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH,
 				((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY))) == 1)) {
 			if (!world.getWorld().isRemote) {
@@ -98,5 +82,7 @@ public class DropDownQuarkProcedure extends ElementaryCraftModElements.ModElemen
 			}
 			world.setBlockState(new BlockPos((int) x, (int) y, (int) z), DownQuarkFieldBlock.block.getDefaultState(), 3);
 		}
+
 	}
+
 }
