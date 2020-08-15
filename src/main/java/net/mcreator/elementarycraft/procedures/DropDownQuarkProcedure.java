c2f9503b0aab6eab36d0ed06d1a3f0500934bc53
MHoutput <Matthew@192.168.0.240>
2020-08-15 18:19:26 +0200

    Comments Tom

--- src/main/java/net/mcreator/elementarycraft/procedures/DropDownQuarkProcedure.java
+++ src/main/java/net/mcreator/elementarycraft/procedures/DropDownQuarkProcedure.java
@@ -1,11 +1,30 @@
 package net.mcreator.elementarycraft.procedures;
 
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.enchantment.EnchantmentHelper;
+
+import net.mcreator.elementarycraft.item.DownQuarkRedItem;
+import net.mcreator.elementarycraft.item.DownQuarkGreenItem;
+import net.mcreator.elementarycraft.item.DownQuarkBlueItem;
+import net.mcreator.elementarycraft.item.AntiDownQuarkRedItem;
+import net.mcreator.elementarycraft.item.AntiDownQuarkGreenItem;
+import net.mcreator.elementarycraft.item.AntiDownQuarkBlueItem;
+import net.mcreator.elementarycraft.block.DownQuarkFieldBlock;
+import net.mcreator.elementarycraft.ElementaryCraftModElements;
+
+import java.util.Map;
+
 @ElementaryCraftModElements.ModElement.Tag
 public class DropDownQuarkProcedure extends ElementaryCraftModElements.ModElement {
-
 	public DropDownQuarkProcedure(ElementaryCraftModElements instance) {
 		super(instance, 15);
-
 	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
@@ -29,60 +48,60 @@ public class DropDownQuarkProcedure extends ElementaryCraftModElements.ModElemen
 			System.err.println("Failed to load dependency world for procedure DropDownQuark!");
 			return;
 		}
-
 		Entity entity = (Entity) dependencies.get("entity");
 		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
 		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
 		IWorld world = (IWorld) dependencies.get("world");
-
-		if (((EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH,
-				((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY))) == 1)) {
-			if (!world.getWorld().isRemote) {
-				ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(DownQuarkFieldBlock.block, (int) (1)));
-				entityToSpawn.setPickupDelay(10);
-				world.addEntity(entityToSpawn);
-			}
-		} else {
-			if ((Math.random() <= 0.333333333333333)) {
+		if ((!((entity instanceof PlayerEntity) ? ((PlayerEntity) entity).abilities.isCreativeMode : false))) {
+			if (((EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH,
+					((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY))) == 1)) {
 				if (!world.getWorld().isRemote) {
-					ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(DownQuarkRedItem.block, (int) (1)));
-					entityToSpawn.setPickupDelay(10);
-					world.addEntity(entityToSpawn);
-				}
-				if (!world.getWorld().isRemote) {
-					ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(AntiDownQuarkRedItem.block, (int) (1)));
+					ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(DownQuarkFieldBlock.block, (int) (1)));
 					entityToSpawn.setPickupDelay(10);
 					world.addEntity(entityToSpawn);
 				}
 			} else {
-				if ((Math.random() <= 0.5)) {
+				if ((Math.random() <= 0.333333333333333)) {
 					if (!world.getWorld().isRemote) {
-						ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(DownQuarkBlueItem.block, (int) (1)));
+						ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(DownQuarkRedItem.block, (int) (1)));
 						entityToSpawn.setPickupDelay(10);
 						world.addEntity(entityToSpawn);
 					}
 					if (!world.getWorld().isRemote) {
-						ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(AntiDownQuarkBlueItem.block, (int) (1)));
+						ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(AntiDownQuarkRedItem.block, (int) (1)));
 						entityToSpawn.setPickupDelay(10);
 						world.addEntity(entityToSpawn);
 					}
 				} else {
-					if (!world.getWorld().isRemote) {
-						ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(DownQuarkGreenItem.block, (int) (1)));
-						entityToSpawn.setPickupDelay(10);
-						world.addEntity(entityToSpawn);
-					}
-					if (!world.getWorld().isRemote) {
-						ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(AntiDownQuarkGreenItem.block, (int) (1)));
-						entityToSpawn.setPickupDelay(10);
-						world.addEntity(entityToSpawn);
+					if ((Math.random() <= 0.5)) {
+						if (!world.getWorld().isRemote) {
+							ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(DownQuarkBlueItem.block, (int) (1)));
+							entityToSpawn.setPickupDelay(10);
+							world.addEntity(entityToSpawn);
+						}
+						if (!world.getWorld().isRemote) {
+							ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z,
+									new ItemStack(AntiDownQuarkBlueItem.block, (int) (1)));
+							entityToSpawn.setPickupDelay(10);
+							world.addEntity(entityToSpawn);
+						}
+					} else {
+						if (!world.getWorld().isRemote) {
+							ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(DownQuarkGreenItem.block, (int) (1)));
+							entityToSpawn.setPickupDelay(10);
+							world.addEntity(entityToSpawn);
+						}
+						if (!world.getWorld().isRemote) {
+							ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z,
+									new ItemStack(AntiDownQuarkGreenItem.block, (int) (1)));
+							entityToSpawn.setPickupDelay(10);
+							world.addEntity(entityToSpawn);
+						}
 					}
 				}
+				world.setBlockState(new BlockPos((int) x, (int) y, (int) z), DownQuarkFieldBlock.block.getDefaultState(), 3);
 			}
-			world.setBlockState(new BlockPos((int) x, (int) y, (int) z), DownQuarkFieldBlock.block.getDefaultState(), 3);
 		}
-
 	}
-
 }
--- src/main/java/net/mcreator/elementarycraft/procedures/DropUpQuarkProcedure.java
+++ src/main/java/net/mcreator/elementarycraft/procedures/DropUpQuarkProcedure.java
@@ -1,11 +1,30 @@
 package net.mcreator.elementarycraft.procedures;
 
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.enchantment.EnchantmentHelper;
+
+import net.mcreator.elementarycraft.item.UpQuarkRedItem;
+import net.mcreator.elementarycraft.item.UpQuarkGreenItem;
+import net.mcreator.elementarycraft.item.UpQuarkBlueItem;
+import net.mcreator.elementarycraft.item.AntiUpQuarkRedItem;
+import net.mcreator.elementarycraft.item.AntiUpQuarkGreenItem;
+import net.mcreator.elementarycraft.item.AntiUpQuarkBlueItem;
+import net.mcreator.elementarycraft.block.UpQuarkFieldBlock;
+import net.mcreator.elementarycraft.ElementaryCraftModElements;
+
+import java.util.Map;
+
 @ElementaryCraftModElements.ModElement.Tag
 public class DropUpQuarkProcedure extends ElementaryCraftModElements.ModElement {
-
 	public DropUpQuarkProcedure(ElementaryCraftModElements instance) {
 		super(instance, 7);
-
 	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
@@ -29,60 +48,59 @@ public class DropUpQuarkProcedure extends ElementaryCraftModElements.ModElement 
 			System.err.println("Failed to load dependency world for procedure DropUpQuark!");
 			return;
 		}
-
 		Entity entity = (Entity) dependencies.get("entity");
 		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
 		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
 		IWorld world = (IWorld) dependencies.get("world");
-
-		if (((EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH,
-				((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY))) == 1)) {
-			if (!world.getWorld().isRemote) {
-				ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(UpQuarkFieldBlock.block, (int) (1)));
-				entityToSpawn.setPickupDelay(10);
-				world.addEntity(entityToSpawn);
-			}
-		} else {
-			if ((Math.random() <= 0.333333333333333)) {
+		if ((!((entity instanceof PlayerEntity) ? ((PlayerEntity) entity).abilities.isCreativeMode : false))) {
+			if (((EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH,
+					((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY))) == 1)) {
 				if (!world.getWorld().isRemote) {
-					ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(UpQuarkRedItem.block, (int) (1)));
-					entityToSpawn.setPickupDelay(10);
-					world.addEntity(entityToSpawn);
-				}
-				if (!world.getWorld().isRemote) {
-					ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(AntiUpQuarkRedItem.block, (int) (1)));
+					ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(UpQuarkFieldBlock.block, (int) (1)));
 					entityToSpawn.setPickupDelay(10);
 					world.addEntity(entityToSpawn);
 				}
 			} else {
-				if ((Math.random() <= 0.5)) {
+				if ((Math.random() <= 0.333333333333333)) {
 					if (!world.getWorld().isRemote) {
-						ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(UpQuarkBlueItem.block, (int) (1)));
+						ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(UpQuarkRedItem.block, (int) (1)));
 						entityToSpawn.setPickupDelay(10);
 						world.addEntity(entityToSpawn);
 					}
 					if (!world.getWorld().isRemote) {
-						ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(AntiUpQuarkBlueItem.block, (int) (1)));
+						ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(AntiUpQuarkRedItem.block, (int) (1)));
 						entityToSpawn.setPickupDelay(10);
 						world.addEntity(entityToSpawn);
 					}
 				} else {
-					if (!world.getWorld().isRemote) {
-						ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(UpQuarkGreenItem.block, (int) (1)));
-						entityToSpawn.setPickupDelay(10);
-						world.addEntity(entityToSpawn);
-					}
-					if (!world.getWorld().isRemote) {
-						ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(AntiUpQuarkGreenItem.block, (int) (1)));
-						entityToSpawn.setPickupDelay(10);
-						world.addEntity(entityToSpawn);
+					if ((Math.random() <= 0.5)) {
+						if (!world.getWorld().isRemote) {
+							ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(UpQuarkBlueItem.block, (int) (1)));
+							entityToSpawn.setPickupDelay(10);
+							world.addEntity(entityToSpawn);
+						}
+						if (!world.getWorld().isRemote) {
+							ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(AntiUpQuarkBlueItem.block, (int) (1)));
+							entityToSpawn.setPickupDelay(10);
+							world.addEntity(entityToSpawn);
+						}
+					} else {
+						if (!world.getWorld().isRemote) {
+							ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(UpQuarkGreenItem.block, (int) (1)));
+							entityToSpawn.setPickupDelay(10);
+							world.addEntity(entityToSpawn);
+						}
+						if (!world.getWorld().isRemote) {
+							ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z,
+									new ItemStack(AntiUpQuarkGreenItem.block, (int) (1)));
+							entityToSpawn.setPickupDelay(10);
+							world.addEntity(entityToSpawn);
+						}
 					}
 				}
+				world.setBlockState(new BlockPos((int) x, (int) y, (int) z), UpQuarkFieldBlock.block.getDefaultState(), 3);
 			}
-			world.setBlockState(new BlockPos((int) x, (int) y, (int) z), UpQuarkFieldBlock.block.getDefaultState(), 3);
 		}
-
 	}
-
 }
