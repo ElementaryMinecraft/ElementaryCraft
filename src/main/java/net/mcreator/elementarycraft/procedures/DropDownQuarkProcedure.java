21771cf664e99aa5c1a025b581cd1712412df011
Matthew <Matthew@192.168.0.240>
2020-08-15 18:19:29 +0200

    MCreator Merge commit '09e6a53df66e594f97e2e8112cdf4e54ce8e70e8' into c2f9503b0aab6eab36d0ed06d1a3f0500934bc53

--- src/main/java/net/mcreator/elementarycraft/procedures/DropDownQuarkProcedure.java
+++ src/main/java/net/mcreator/elementarycraft/procedures/DropDownQuarkProcedure.java
@@ -1,30 +1,11 @@
 package net.mcreator.elementarycraft.procedures;
 
-import net.minecraft.world.IWorld;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.item.ItemStack;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.item.ItemEntity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.Entity;
-import net.minecraft.enchantment.Enchantments;
-import net.minecraft.enchantment.EnchantmentHelper;
-
-import net.mcreator.elementarycraft.item.DownQuarkRedItem;
-import net.mcreator.elementarycraft.item.DownQuarkGreenItem;
-import net.mcreator.elementarycraft.item.DownQuarkBlueItem;
-import net.mcreator.elementarycraft.item.AntiDownQuarkRedItem;
-import net.mcreator.elementarycraft.item.AntiDownQuarkGreenItem;
-import net.mcreator.elementarycraft.item.AntiDownQuarkBlueItem;
-import net.mcreator.elementarycraft.block.DownQuarkFieldBlock;
-import net.mcreator.elementarycraft.ElementaryCraftModElements;
-
-import java.util.Map;
-
 @ElementaryCraftModElements.ModElement.Tag
 public class DropDownQuarkProcedure extends ElementaryCraftModElements.ModElement {
+
 	public DropDownQuarkProcedure(ElementaryCraftModElements instance) {
 		super(instance, 15);
+
 	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
@@ -48,16 +29,18 @@ public class DropDownQuarkProcedure extends ElementaryCraftModElements.ModElemen
 			System.err.println("Failed to load dependency world for procedure DropDownQuark!");
 			return;
 		}
+
 		Entity entity = (Entity) dependencies.get("entity");
 		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
 		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
 		IWorld world = (IWorld) dependencies.get("world");
+
 		if ((!((entity instanceof PlayerEntity) ? ((PlayerEntity) entity).abilities.isCreativeMode : false))) {
 			if (((EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH,
 					((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY))) == 1)) {
 				if (!world.getWorld().isRemote) {
-					ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(DownQuarkFieldBlock.block, (int) (1)));
+					ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(DownQuarkFieldItem.block, (int) (1)));
 					entityToSpawn.setPickupDelay(10);
 					world.addEntity(entityToSpawn);
 				}
@@ -100,8 +83,10 @@ public class DropDownQuarkProcedure extends ElementaryCraftModElements.ModElemen
 						}
 					}
 				}
-				world.setBlockState(new BlockPos((int) x, (int) y, (int) z), DownQuarkFieldBlock.block.getDefaultState(), 3);
+				world.setBlockState(new BlockPos((int) x, (int) y, (int) z), DownQuarkFieldItem.block.getDefaultState(), 3);
 			}
 		}
+
 	}
+
 }
