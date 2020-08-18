3bf83b7b85de7f33abb7b0bcade1e19ca2960e26
Ine Arts <s0142398@ad.ua.ac.be>
2020-08-18 19:41:53 +0200

    MCreator Merge commit '41a6da8ac29707fa56e537cccf069ea4a61682e2' into dd0c43ecdaba0052259f4f843f381f93555542e6

--- src/main/java/net/mcreator/elementarycraft/item/GluonItem.java
+++ src/main/java/net/mcreator/elementarycraft/item/GluonItem.java
@@ -1,25 +1,12 @@
 
 package net.mcreator.elementarycraft.item;
 
-import net.minecraftforge.registries.ObjectHolder;
-
-import net.minecraft.world.World;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.Item;
-import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.block.BlockState;
-
-import net.mcreator.elementarycraft.ElementaryCraftModElements;
-
-import java.util.List;
-
 @ElementaryCraftModElements.ModElement.Tag
 public class GluonItem extends ElementaryCraftModElements.ModElement {
+
 	@ObjectHolder("elementary_craft:gluon")
 	public static final Item block = null;
+
 	public GluonItem(ElementaryCraftModElements instance) {
 		super(instance, 1);
 	}
@@ -28,7 +15,9 @@ public class GluonItem extends ElementaryCraftModElements.ModElement {
 	public void initElements() {
 		elements.items.add(() -> new ItemCustom());
 	}
+
 	public static class ItemCustom extends Item {
+
 		public ItemCustom() {
 			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64));
 			setRegistryName("gluon");
@@ -54,5 +43,7 @@ public class GluonItem extends ElementaryCraftModElements.ModElement {
 			super.addInformation(itemstack, world, list, flag);
 			list.add(new StringTextComponent("a gluon is the exchange particle for the strong force between quarks"));
 		}
+
 	}
+
 }
