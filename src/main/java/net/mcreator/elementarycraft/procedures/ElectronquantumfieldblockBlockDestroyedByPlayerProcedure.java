f670e14d3a0b3c7541b472df0d566653c27b84eb
TimourIc <User@192.168.0.135>
2020-08-23 17:51:37 +0200

    De if statement voor de creative mode toegevoegd.

--- src/main/java/net/mcreator/elementarycraft/procedures/ElectronquantumfieldblockBlockDestroyedByPlayerProcedure.java
+++ src/main/java/net/mcreator/elementarycraft/procedures/ElectronquantumfieldblockBlockDestroyedByPlayerProcedure.java
@@ -3,6 +3,7 @@ package net.mcreator.elementarycraft.procedures;
 import net.minecraft.world.IWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.Entity;
@@ -48,25 +49,28 @@ public class ElectronquantumfieldblockBlockDestroyedByPlayerProcedure extends El
 		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
 		IWorld world = (IWorld) dependencies.get("world");
-		if (((EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH,
-				((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY))) == 1)) {
-			if (!world.getWorld().isRemote) {
-				ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(ElectronquantumfieldblockBlock.block, (int) (1)));
-				entityToSpawn.setPickupDelay(10);
-				world.addEntity(entityToSpawn);
+		if ((!((entity instanceof PlayerEntity) ? ((PlayerEntity) entity).abilities.isCreativeMode : false))) {
+			if (((EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH,
+					((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY))) == 1)) {
+				if (!world.getWorld().isRemote) {
+					ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z,
+							new ItemStack(ElectronquantumfieldblockBlock.block, (int) (1)));
+					entityToSpawn.setPickupDelay(10);
+					world.addEntity(entityToSpawn);
+				}
+			} else {
+				if (!world.getWorld().isRemote) {
+					ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(ElectronItem.block, (int) (1)));
+					entityToSpawn.setPickupDelay(10);
+					world.addEntity(entityToSpawn);
+				}
+				if (!world.getWorld().isRemote) {
+					ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(PositronItem.block, (int) (1)));
+					entityToSpawn.setPickupDelay(10);
+					world.addEntity(entityToSpawn);
+				}
+				world.setBlockState(new BlockPos((int) x, (int) y, (int) z), ElectronquantumfieldblockBlock.block.getDefaultState(), 3);
 			}
-		} else {
-			if (!world.getWorld().isRemote) {
-				ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(ElectronItem.block, (int) (1)));
-				entityToSpawn.setPickupDelay(10);
-				world.addEntity(entityToSpawn);
-			}
-			if (!world.getWorld().isRemote) {
-				ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(PositronItem.block, (int) (1)));
-				entityToSpawn.setPickupDelay(10);
-				world.addEntity(entityToSpawn);
-			}
-			world.setBlockState(new BlockPos((int) x, (int) y, (int) z), ElectronquantumfieldblockBlock.block.getDefaultState(), 3);
 		}
 	}
 }
