9ed65a9032ebd6505fce6ee2fab166c151080cd2
IneMEGAmaxi <s0142398@ad.ua.ac.be>
2020-08-24 21:09:48 +0200

    texture quark fields

--- src/main/java/net/mcreator/elementarycraft/item/AntiUpQuarkRedItem.java
+++ src/main/java/net/mcreator/elementarycraft/item/AntiUpQuarkRedItem.java
@@ -1,12 +1,19 @@
 
 package net.mcreator.elementarycraft.item;
 
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.elementarycraft.ElementaryCraftModElements;
+
 @ElementaryCraftModElements.ModElement.Tag
 public class AntiUpQuarkRedItem extends ElementaryCraftModElements.ModElement {
-
 	@ObjectHolder("elementary_craft:anti_up_quark_red")
 	public static final Item block = null;
-
 	public AntiUpQuarkRedItem(ElementaryCraftModElements instance) {
 		super(instance, 4);
 	}
@@ -15,9 +22,7 @@ public class AntiUpQuarkRedItem extends ElementaryCraftModElements.ModElement {
 	public void initElements() {
 		elements.items.add(() -> new ItemCustom());
 	}
-
 	public static class ItemCustom extends Item {
-
 		public ItemCustom() {
 			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64));
 			setRegistryName("anti_up_quark_red");
@@ -37,7 +42,5 @@ public class AntiUpQuarkRedItem extends ElementaryCraftModElements.ModElement {
 		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
 			return 1F;
 		}
-
 	}
-
 }
