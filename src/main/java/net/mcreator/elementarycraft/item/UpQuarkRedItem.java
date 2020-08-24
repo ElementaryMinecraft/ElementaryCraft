c1b8bee36d2afe220f19147d0fb67c1c8f74c1da
Ine Arts <s0142398@ad.ua.ac.be>
2020-08-24 22:43:33 +0200

    MCreator Merge commit '616edb06071bbd5c5a3f338b886309b2a7ee3d61' into 8b8fd4a5d6fddba579c053f9f34f90a4e1c289c8

--- src/main/java/net/mcreator/elementarycraft/item/UpQuarkRedItem.java
+++ src/main/java/net/mcreator/elementarycraft/item/UpQuarkRedItem.java
@@ -1,19 +1,12 @@
 
 package net.mcreator.elementarycraft.item;
 
-import net.minecraftforge.registries.ObjectHolder;
-
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.Item;
-import net.minecraft.block.BlockState;
-
-import net.mcreator.elementarycraft.ElementaryCraftModElements;
-
 @ElementaryCraftModElements.ModElement.Tag
 public class UpQuarkRedItem extends ElementaryCraftModElements.ModElement {
+
 	@ObjectHolder("elementary_craft:up_quark_red")
 	public static final Item block = null;
+
 	public UpQuarkRedItem(ElementaryCraftModElements instance) {
 		super(instance, 1);
 	}
@@ -22,7 +15,9 @@ public class UpQuarkRedItem extends ElementaryCraftModElements.ModElement {
 	public void initElements() {
 		elements.items.add(() -> new ItemCustom());
 	}
+
 	public static class ItemCustom extends Item {
+
 		public ItemCustom() {
 			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64));
 			setRegistryName("up_quark_red");
@@ -42,5 +37,7 @@ public class UpQuarkRedItem extends ElementaryCraftModElements.ModElement {
 		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
 			return 1F;
 		}
+
 	}
+
 }
--- src/main/java/net/mcreator/elementarycraft/procedures/DropDownQuarkProcedure.java
+++ src/main/java/net/mcreator/elementarycraft/procedures/DropDownQuarkProcedure.java
@@ -1,30 +1,11 @@
 package net.mcreator.elementarycraft.procedures;
 
-import net.minecraft.world.IWorld;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.item.ItemStack;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.item.ItemEntity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.Entity;
-import net.minecraft.enchantment.Enchantments;
-import net.minecraft.enchantment.EnchantmentHelper;
-
-import net.mcreator.elementarycraft.item.DownQuarkRedItem;
-import net.mcreator.elementarycraft.item.DownQuarkGreenItem;
-import net.mcreator.elementarycraft.item.DownQuarkBlueItem;
-import net.mcreator.elementarycraft.item.AntiDownQuarkRedItem;
-import net.mcreator.elementarycraft.item.AntiDownQuarkGreenItem;
-import net.mcreator.elementarycraft.item.AntiDownQuarkBlueItem;
-import net.mcreator.elementarycraft.block.DownQuarkFieldBlock;
-import net.mcreator.elementarycraft.ElementaryCraftModElements;
-
-import java.util.Map;
-
 @ElementaryCraftModElements.ModElement.Tag
 public class DropDownQuarkProcedure extends ElementaryCraftModElements.ModElement {
+
 	public DropDownQuarkProcedure(ElementaryCraftModElements instance) {
 		super(instance, 15);
+
 	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
@@ -48,11 +29,13 @@ public class DropDownQuarkProcedure extends ElementaryCraftModElements.ModElemen
 			System.err.println("Failed to load dependency world for procedure DropDownQuark!");
 			return;
 		}
+
 		Entity entity = (Entity) dependencies.get("entity");
 		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
 		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
 		IWorld world = (IWorld) dependencies.get("world");
+
 		if ((!((entity instanceof PlayerEntity) ? ((PlayerEntity) entity).abilities.isCreativeMode : false))) {
 			if (((EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH,
 					((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY))) == 1)) {
@@ -103,5 +86,7 @@ public class DropDownQuarkProcedure extends ElementaryCraftModElements.ModElemen
 				world.setBlockState(new BlockPos((int) x, (int) y, (int) z), DownQuarkFieldBlock.block.getDefaultState(), 3);
 			}
 		}
+
 	}
+
 }
