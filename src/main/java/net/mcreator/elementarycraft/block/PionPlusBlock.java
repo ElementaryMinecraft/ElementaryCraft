a7478a9ecd92537495aedbc7d3b6e6cfffdc6abb
MHoutput <matthew.houtput@uantwerpen.be>
2020-08-29 18:44:03 +0200

    MCreator Merge commit '1d86e65307f59f851f1d01a326c5fd020359ec98' into b25fd9ede391f6f176386b59e37e20b4bc595d80

--- src/main/java/net/mcreator/elementarycraft/block/PionPlusBlock.java
+++ src/main/java/net/mcreator/elementarycraft/block/PionPlusBlock.java
@@ -1,36 +1,17 @@
 
 package net.mcreator.elementarycraft.block;
 
-import net.minecraftforge.registries.ObjectHolder;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.api.distmarker.Dist;
-
-import net.minecraft.world.storage.loot.LootContext;
-import net.minecraft.world.IBlockReader;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Item;
-import net.minecraft.item.BlockItem;
-import net.minecraft.client.renderer.RenderTypeLookup;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.SoundType;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Block;
-
-import net.mcreator.elementarycraft.itemgroup.ElementaryParticleItemGroup;
-import net.mcreator.elementarycraft.ElementaryCraftModElements;
-
-import java.util.List;
-import java.util.Collections;
 
 @ElementaryCraftModElements.ModElement.Tag
 public class PionPlusBlock extends ElementaryCraftModElements.ModElement {
+
 	@ObjectHolder("elementary_craft:pion_plus")
 	public static final Block block = null;
+
 	public PionPlusBlock(ElementaryCraftModElements instance) {
 		super(instance, 24);
+
 	}
 
 	@Override
@@ -45,10 +26,15 @@ public class PionPlusBlock extends ElementaryCraftModElements.ModElement {
 	public void clientLoad(FMLClientSetupEvent event) {
 		RenderTypeLookup.setRenderLayer(block, RenderType.getTranslucent());
 	}
+
 	public static class CustomBlock extends Block {
+
 		public CustomBlock() {
-			super(Block.Properties.create(Material.ROCK).sound(SoundType.CLOTH).hardnessAndResistance(1f, 10f).lightValue(0).doesNotBlockMovement()
-					.notSolid());
+			super(
+
+					Block.Properties.create(Material.ROCK).sound(SoundType.CLOTH).hardnessAndResistance(1f, 10f).lightValue(0).doesNotBlockMovement()
+							.notSolid());
+
 			setRegistryName("pion_plus");
 		}
 
@@ -59,10 +45,13 @@ public class PionPlusBlock extends ElementaryCraftModElements.ModElement {
 
 		@Override
 		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+
 			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 			if (!dropsOriginal.isEmpty())
 				return dropsOriginal;
 			return Collections.singletonList(new ItemStack(this, 1));
 		}
+
 	}
+
 }
