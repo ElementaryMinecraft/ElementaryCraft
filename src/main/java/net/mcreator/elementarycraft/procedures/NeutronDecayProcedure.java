61983f310d6b2a74d20d5324741d36c43a5d5d9d
MajaVerstraeten <69113323+MajaVerstraeten@users.noreply.github.com>
2020-09-08 20:36:25 +0200

    Revert "Quarkdye"

--- src/main/java/net/mcreator/elementarycraft/procedures/NeutronDecayProcedure.java
+++ src/main/java/net/mcreator/elementarycraft/procedures/NeutronDecayProcedure.java
@@ -1,11 +1,22 @@
 package net.mcreator.elementarycraft.procedures;
 
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.state.IProperty;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.elementarycraft.block.WeakBosonBlock;
+import net.mcreator.elementarycraft.block.ProtonBlock;
+import net.mcreator.elementarycraft.ElementaryCraftModElements;
+
+import java.util.Map;
+
 @ElementaryCraftModElements.ModElement.Tag
 public class NeutronDecayProcedure extends ElementaryCraftModElements.ModElement {
-
 	public NeutronDecayProcedure(ElementaryCraftModElements instance) {
 		super(instance, 55);
-
 	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
@@ -25,26 +36,21 @@ public class NeutronDecayProcedure extends ElementaryCraftModElements.ModElement
 			System.err.println("Failed to load dependency world for procedure NeutronDecay!");
 			return;
 		}
-
 		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
 		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
 		IWorld world = (IWorld) dependencies.get("world");
-
 		if ((Math.random() < 0.002768748)) {
 			{
 				BlockPos _bp = new BlockPos((int) x, (int) y, (int) z);
 				BlockState _bs = ProtonBlock.block.getDefaultState();
-
 				BlockState _bso = world.getBlockState(_bp);
 				for (Map.Entry<IProperty<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
 					IProperty _property = _bs.getBlock().getStateContainer().getProperty(entry.getKey().getName());
 					if (_bs.has(_property))
 						_bs = _bs.with(_property, (Comparable) entry.getValue());
 				}
-
 				world.setBlockState(_bp, _bs, 3);
-
 			}
 			if (!world.getWorld().isRemote) {
 				ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(WeakBosonBlock.block, (int) (1)));
@@ -52,7 +58,5 @@ public class NeutronDecayProcedure extends ElementaryCraftModElements.ModElement
 				world.addEntity(entityToSpawn);
 			}
 		}
-
 	}
-
 }
