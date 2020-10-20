ca33e7d70b5eec03cf68a4cb769535e306288f99
Bert Jorissen <info@bertjorissen.be>
2020-10-20 14:57:38 +0200

    Farms with Torches
    The farms for quantum fields with torches are iplemented for up/down quarks and neutrino's. Also, the 'drop' or 'destroy' procedure is rewritten to save duplication of code.

--- src/main/java/net/mcreator/elementarycraft/procedures/DropDownQuarkProcedure.java
+++ src/main/java/net/mcreator/elementarycraft/procedures/DropDownQuarkProcedure.java
@@ -1,14 +1,8 @@
 package net.mcreator.elementarycraft.procedures;
 
 import net.minecraft.world.IWorld;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.item.ItemStack;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.item.ItemEntity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.Entity;
-import net.minecraft.enchantment.Enchantments;
-import net.minecraft.enchantment.EnchantmentHelper;
 
 import net.mcreator.elementarycraft.item.DownQuarkRedItem;
 import net.mcreator.elementarycraft.item.DownQuarkGreenItem;
@@ -16,7 +10,6 @@ import net.mcreator.elementarycraft.item.DownQuarkBlueItem;
 import net.mcreator.elementarycraft.item.AntiDownQuarkRedItem;
 import net.mcreator.elementarycraft.item.AntiDownQuarkGreenItem;
 import net.mcreator.elementarycraft.item.AntiDownQuarkBlueItem;
-import net.mcreator.elementarycraft.block.DownQuarkFieldBlock;
 import net.mcreator.elementarycraft.ElementaryCraftModElements;
 
 import java.util.Map;
@@ -28,10 +21,6 @@ public class DropDownQuarkProcedure extends ElementaryCraftModElements.ModElemen
 	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
-		if (dependencies.get("entity") == null) {
-			System.err.println("Failed to load dependency entity for procedure DropDownQuark!");
-			return;
-		}
 		if (dependencies.get("x") == null) {
 			System.err.println("Failed to load dependency x for procedure DropDownQuark!");
 			return;
@@ -48,59 +37,44 @@ public class DropDownQuarkProcedure extends ElementaryCraftModElements.ModElemen
 			System.err.println("Failed to load dependency world for procedure DropDownQuark!");
 			return;
 		}
-		Entity entity = (Entity) dependencies.get("entity");
 		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
 		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
 		IWorld world = (IWorld) dependencies.get("world");
-		if ((!((entity instanceof PlayerEntity) ? ((PlayerEntity) entity).abilities.isCreativeMode : false))) {
-			if (((EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH,
-					((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY))) == 1)) {
+		if ((Math.random() <= 0.333333333333333)) {
+			if (!world.getWorld().isRemote) {
+				ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(DownQuarkBlueItem.block, (int) (1)));
+				entityToSpawn.setPickupDelay(10);
+				world.addEntity(entityToSpawn);
+			}
+			if (!world.getWorld().isRemote) {
+				ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(AntiDownQuarkBlueItem.block, (int) (1)));
+				entityToSpawn.setPickupDelay(10);
+				world.addEntity(entityToSpawn);
+			}
+		} else {
+			if ((Math.random() <= 0.5)) {
 				if (!world.getWorld().isRemote) {
-					ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(DownQuarkFieldBlock.block, (int) (1)));
+					ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(DownQuarkGreenItem.block, (int) (1)));
+					entityToSpawn.setPickupDelay(10);
+					world.addEntity(entityToSpawn);
+				}
+				if (!world.getWorld().isRemote) {
+					ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(AntiDownQuarkGreenItem.block, (int) (1)));
 					entityToSpawn.setPickupDelay(10);
 					world.addEntity(entityToSpawn);
 				}
 			} else {
-				if ((Math.random() <= 0.333333333333333)) {
-					if (!world.getWorld().isRemote) {
-						ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(DownQuarkRedItem.block, (int) (1)));
-						entityToSpawn.setPickupDelay(10);
-						world.addEntity(entityToSpawn);
-					}
-					if (!world.getWorld().isRemote) {
-						ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(AntiDownQuarkRedItem.block, (int) (1)));
-						entityToSpawn.setPickupDelay(10);
-						world.addEntity(entityToSpawn);
-					}
-				} else {
-					if ((Math.random() <= 0.5)) {
-						if (!world.getWorld().isRemote) {
-							ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(DownQuarkBlueItem.block, (int) (1)));
-							entityToSpawn.setPickupDelay(10);
-							world.addEntity(entityToSpawn);
-						}
-						if (!world.getWorld().isRemote) {
-							ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z,
-									new ItemStack(AntiDownQuarkBlueItem.block, (int) (1)));
-							entityToSpawn.setPickupDelay(10);
-							world.addEntity(entityToSpawn);
-						}
-					} else {
-						if (!world.getWorld().isRemote) {
-							ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(DownQuarkGreenItem.block, (int) (1)));
-							entityToSpawn.setPickupDelay(10);
-							world.addEntity(entityToSpawn);
-						}
-						if (!world.getWorld().isRemote) {
-							ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z,
-									new ItemStack(AntiDownQuarkGreenItem.block, (int) (1)));
-							entityToSpawn.setPickupDelay(10);
-							world.addEntity(entityToSpawn);
-						}
-					}
+				if (!world.getWorld().isRemote) {
+					ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(DownQuarkRedItem.block, (int) (1)));
+					entityToSpawn.setPickupDelay(10);
+					world.addEntity(entityToSpawn);
 				}
-				world.setBlockState(new BlockPos((int) x, (int) y, (int) z), DownQuarkFieldBlock.block.getDefaultState(), 3);
+				if (!world.getWorld().isRemote) {
+					ItemEntity entityToSpawn = new ItemEntity(world.getWorld(), x, y, z, new ItemStack(AntiDownQuarkRedItem.block, (int) (1)));
+					entityToSpawn.setPickupDelay(10);
+					world.addEntity(entityToSpawn);
+				}
 			}
 		}
 	}
---
+++ src/main/java/net/mcreator/elementarycraft/procedures/TorchTickProcedure.java
@@ -0,0 +1,85 @@
+package net.mcreator.elementarycraft.procedures;
+
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.tileentity.TileEntity;
+
+import net.mcreator.elementarycraft.block.UpQuarkFieldBlock;
+import net.mcreator.elementarycraft.block.NeutrinoQuantumfieldBlock;
+import net.mcreator.elementarycraft.block.DownQuarkFieldBlock;
+import net.mcreator.elementarycraft.ElementaryCraftModElements;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@ElementaryCraftModElements.ModElement.Tag
+public class TorchTickProcedure extends ElementaryCraftModElements.ModElement {
+	public TorchTickProcedure(ElementaryCraftModElements instance) {
+		super(instance, 103);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("x") == null) {
+			System.err.println("Failed to load dependency x for procedure TorchTick!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			System.err.println("Failed to load dependency y for procedure TorchTick!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			System.err.println("Failed to load dependency z for procedure TorchTick!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			System.err.println("Failed to load dependency world for procedure TorchTick!");
+			return;
+		}
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		if ((new Object() {
+			public boolean getValue(BlockPos pos, String tag) {
+				TileEntity tileEntity = world.getTileEntity(pos);
+				if (tileEntity != null)
+					return tileEntity.getTileData().getBoolean(tag);
+				return false;
+			}
+		}.getValue(new BlockPos((int) x, (int) y, (int) z), "onTorch"))) {
+			if ((Math.random() <= 0.25)) {
+				if (((world.getBlockState(new BlockPos((int) x, (int) y, (int) z))).getBlock() == NeutrinoQuantumfieldBlock.block.getDefaultState()
+						.getBlock())) {
+					{
+						Map<String, Object> $_dependencies = new HashMap<>();
+						$_dependencies.put("x", x);
+						$_dependencies.put("y", y);
+						$_dependencies.put("z", z);
+						$_dependencies.put("world", world);
+						NeutrinoQuantumfieldDropProcedure.executeProcedure($_dependencies);
+					}
+				} else if (((world.getBlockState(new BlockPos((int) x, (int) y, (int) z))).getBlock() == DownQuarkFieldBlock.block.getDefaultState()
+						.getBlock())) {
+					{
+						Map<String, Object> $_dependencies = new HashMap<>();
+						$_dependencies.put("x", x);
+						$_dependencies.put("y", y);
+						$_dependencies.put("z", z);
+						$_dependencies.put("world", world);
+						DropDownQuarkProcedure.executeProcedure($_dependencies);
+					}
+				} else if (((world.getBlockState(new BlockPos((int) x, (int) y, (int) z))).getBlock() == UpQuarkFieldBlock.block.getDefaultState()
+						.getBlock())) {
+					{
+						Map<String, Object> $_dependencies = new HashMap<>();
+						$_dependencies.put("x", x);
+						$_dependencies.put("y", y);
+						$_dependencies.put("z", z);
+						$_dependencies.put("world", world);
+						DropUpQuarkProcedure.executeProcedure($_dependencies);
+					}
+				}
+			}
+		}
+	}
+}
