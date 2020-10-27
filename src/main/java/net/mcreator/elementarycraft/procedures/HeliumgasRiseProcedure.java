23236aabb7844d84c5f715967b0821d5bbb20199
MajaVerstraeten <maja.verstraeten@uantwerpen.be>
2020-10-27 21:58:36 +0100

    helium rise

---
+++ src/main/java/net/mcreator/elementarycraft/procedures/HeliumgasRiseProcedure.java
@@ -0,0 +1,48 @@
+package net.mcreator.elementarycraft.procedures;
+
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.block.Blocks;
+
+import net.mcreator.elementarycraft.block.HeliumgasBlock;
+import net.mcreator.elementarycraft.ElementaryCraftModElements;
+
+import java.util.Map;
+
+@ElementaryCraftModElements.ModElement.Tag
+public class HeliumgasRiseProcedure extends ElementaryCraftModElements.ModElement {
+	public HeliumgasRiseProcedure(ElementaryCraftModElements instance) {
+		super(instance, 116);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("x") == null) {
+			System.err.println("Failed to load dependency x for procedure HeliumgasRise!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			System.err.println("Failed to load dependency y for procedure HeliumgasRise!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			System.err.println("Failed to load dependency z for procedure HeliumgasRise!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			System.err.println("Failed to load dependency world for procedure HeliumgasRise!");
+			return;
+		}
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		if ((Math.random() < 0.1)) {
+			world.setBlockState(new BlockPos((int) x, (int) y, (int) z), Blocks.AIR.getDefaultState(), 3);
+		} else {
+			if (((world.getBlockState(new BlockPos((int) x, (int) (y + 1), (int) z))).getMaterial() == net.minecraft.block.material.Material.AIR)) {
+				world.setBlockState(new BlockPos((int) x, (int) y, (int) z), Blocks.AIR.getDefaultState(), 3);
+				world.setBlockState(new BlockPos((int) x, (int) (y + 1), (int) z), HeliumgasBlock.block.getDefaultState(), 3);
+			}
+		}
+	}
+}
