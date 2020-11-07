b13290a9695e58bf3ff0fefe382bc35356be5b7a
MajaVerstraeten <maja.verstraeten@uantwerpen.be>
2020-11-07 20:59:25 +0100

    update balloon

--- src/main/java/net/mcreator/elementarycraft/item/BalloonItem.java
+++ src/main/java/net/mcreator/elementarycraft/item/BalloonItem.java
@@ -3,18 +3,12 @@ package net.mcreator.elementarycraft.item;
 
 import net.minecraftforge.registries.ObjectHolder;
 
-import net.minecraft.world.World;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
-import net.minecraft.entity.Entity;
 import net.minecraft.block.BlockState;
 
-import net.mcreator.elementarycraft.procedures.BalloonInHandProcedure;
 import net.mcreator.elementarycraft.ElementaryCraftModElements;
-
-import java.util.Map;
-import java.util.HashMap;
 
 @ElementaryCraftModElements.ModElement.Tag
 public class BalloonItem extends ElementaryCraftModElements.ModElement {
@@ -47,19 +41,6 @@ public class BalloonItem extends ElementaryCraftModElements.ModElement {
 		@Override
 		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
 			return 1F;
-		}
-
-		@Override
-		public void inventoryTick(ItemStack itemstack, World world, Entity entity, int slot, boolean selected) {
-			super.inventoryTick(itemstack, world, entity, slot, selected);
-			double x = entity.getPosX();
-			double y = entity.getPosY();
-			double z = entity.getPosZ();
-			if (selected) {
-				Map<String, Object> $_dependencies = new HashMap<>();
-				$_dependencies.put("entity", entity);
-				BalloonInHandProcedure.executeProcedure($_dependencies);
-			}
 		}
 	}
 }
