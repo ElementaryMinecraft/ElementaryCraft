ac755d9bba9c147c36de50bf4d7ba42c4330f12a
MajaVerstraeten <maja.verstraeten@uantwerpen.be>
2020-12-11 17:13:25 +0100

    forgot the untracked files

---
+++ src/main/java/net/mcreator/elementarycraft/procedures/PionPlusBlockAddedProcedure.java
@@ -0,0 +1,58 @@
+package net.mcreator.elementarycraft.procedures;
+
+import net.minecraft.world.IWorld;
+
+import net.mcreator.elementarycraft.ElementaryCraftModElements;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@ElementaryCraftModElements.ModElement.Tag
+public class PionPlusBlockAddedProcedure extends ElementaryCraftModElements.ModElement {
+	public PionPlusBlockAddedProcedure(ElementaryCraftModElements instance) {
+		super(instance, 129);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				System.err.println("Failed to load dependency x for procedure PionPlusBlockAdded!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				System.err.println("Failed to load dependency y for procedure PionPlusBlockAdded!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				System.err.println("Failed to load dependency z for procedure PionPlusBlockAdded!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				System.err.println("Failed to load dependency world for procedure PionPlusBlockAdded!");
+			return;
+		}
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		{
+			Map<String, Object> $_dependencies = new HashMap<>();
+			$_dependencies.put("world", world);
+			$_dependencies.put("x", x);
+			$_dependencies.put("y", y);
+			$_dependencies.put("z", z);
+			PositiveChargedAddProcedure.executeProcedure($_dependencies);
+		}
+		{
+			Map<String, Object> $_dependencies = new HashMap<>();
+			$_dependencies.put("world", world);
+			$_dependencies.put("x", x);
+			$_dependencies.put("y", y);
+			$_dependencies.put("z", z);
+			PionMinecartProcedure.executeProcedure($_dependencies);
+		}
+	}
+}
